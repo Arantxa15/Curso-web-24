87edfd577a4fb5656f92bed78a6efb90a477bad4
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-05-24 11:27:46 +0200

    24 de mayo

---
+++ js/bucles.js
@@ -0,0 +1,24 @@
+console.log('Bucles');
+
+// Los bucles son operaciones que se repiten un numero determinado de veces (vueltas)
+// Son muy útiles cuando queremos ejecutar el mismo código repetidamente, pero cada vez con un valor diferente
+
+// ---FOR---
+// Esta declaración crea una expresión con tres valores opcionales:
+
+// 1. Indica que el inicio del conteo del bucle (normalmente 0 o 1) 
+
+// 2. Indica la condición para que se ejecute el bucle (y por tanto el número de veces)
+
+// 3. Indica la dirección del conteo. Aumenta o disminuye (cuenta átras)
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
--- js/metodos.js
+++ js/metodos.js
@@ -35,8 +35,102 @@ let primeraLetra = texto.charAt(0);
 
 console.log(primeraLetra);
 
-let ultimaLetra = texto.charAt(texto.length -1);
+let ultimaLetra = texto.charAt(texto.length - 1);
 console.log(ultimaLetra);
 
-let letraM = texto.charAt(texto.length -5);
-console.log(letraM); 
+let letraM = texto.charAt(texto.length - 5);
+console.log(letraM);
+
+
+
+// AT
+// En principio funciona como charAt():
+texto = 'Feliz viernes!';
+
+primeraLetra = texto.at(0);
+console.log(primeraLetra);
+
+// Excepto cuando contamos desde el final:
+// charAt() no admite números negativos en el índicepero at() si
+
+ultimaLetra = texto.at(-1);
+console.log(ultimaLetra);
+
+let letraR = texto.at(-5);
+console.log(letraR);
+
+// Un sitring también se puede leer como un array
+let letra = texto[6];
+console.log(letra);
+
+// ----Extraer trozos de un string-----
+// slice(inicio, final)
+
+// Retorna un nuevo string usando las posiciones inicial y final con parámetros
+texto = "Yogur, Donettes, Patatas";
+let trozo = texto.slice(7, 15);
+console.log(trozo);
+
+// Si omitimos el segundo parámetro, slice conrta el resto del string:
+trozo = texto.slice(7);
+console.log(trozo);
+
+// Slice() admite valores negativos:
+// Con valores negativos cuenta desde el final hacia la izquierda
+trozo = texto.slice(-7);
+console.log(trozo);
+
+// Si el segundo parámetro es negativo, cuenta desde la posición inicial hacia la izquierda:
+trozo = texto.slice(-17, -9);
+console.log(trozo);
+
+
+// ----Transsformar un string----
+// A mayúsculas:
+let textoMayus = texto.toUpperCase();
+console.log(textoMayus);
+
+// A minúsculas:
+let textoMinus = texto.toLowerCase();
+console.log(textoMinus);
+
+// ----Concatenar (unir) textos----
+let nombre = "Caroline";
+let apellido = "García";
+
+let nombreCompleto = nombre + " " + apellido;
+// let nombreCompleto = nombre.concat(" ", apellido);
+
+
+console.log(nombreCompleto);
+
+// ----Limpiar strings----
+
+// trim() elimina los espacios en blanco al inicio y al final de un string
+texto = "    Me llamo Arantxa     ";
+console.log(texto);
+console.log(texto.length);
+
+// A veces es necesario limpiar los strings para , por ejemplo, guardar estos datos en una base de datos
+let textoLimpio = texto.trim();
+console.log(textoLimpio);
+console.log(textoLimpio.length);
+
+// ----Reemplazar strings----
+texto = textoLimpio;
+console.log(texto);
+
+texto = texto.replace("Arantxa", "Elizabeth");
+console.log(texto);
+
+// ----Convertir un string en un array----
+// split(separador)
+texto = "Yogur, Donettes, Patatas";
+const listaCompra = texto.split(", ");
+
+console.log(listaCompra);
+
+// ----Búsquedas en string---
+// Search retorna el índice del primer caracter del texto buscado:
+let posiciónDonettes = texto.search("Donettes");
+console.log(posiciónDonettes);
