0b0385be7371a2ac53775480d3b430fcb1d3c1aa
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-06-21 11:33:47 +0200

    21 de junio

--- js/DOM/form_validation/contenedores.js
+++ js/DOM/form_validation/contenedores.js
@@ -3,12 +3,20 @@ const divs = document.querySelectorAll('.contenedor div');
 
 for (let i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', function() {
+
+        // guardamos 'this' en una variable
+        // this se refiere al objeto que interactua en cada caso
         let cuadro = this;
+        // le cambiamos el color del fondo
         cuadro.style.backgroundColor = 'dodgerblue';
 
+        // ponemos un tempòrizador para que el div vuelva a su color inicial
         setTimeout(function() {
+
+            // desde aqui dentro no podemos acceder a 'this', por eso lo hemos guardado en una variable
             cuadro.style.backgroundColor = 'white';
         }, 1500);
+        // 1500ms = 1.5s -> el valor de transition que le dimos en CSS
     })
     
 }
